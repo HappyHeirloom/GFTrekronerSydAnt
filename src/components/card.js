fb257050abfe3b5262108e6a6e0765b79b26dc73
Rasmus Deckmann <rasm732d@edu.easj.dk>
2021-09-23 11:21:23 +0200

    Cards fra material UI

--- src/components/card.js
+++ src/components/card.js
@@ -1,23 +1,59 @@
 //#region Imports
 import React from "react";
 
+// Material UI
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Typography from "@material-ui/core/Typography";
+
 //#endregion
 
 //#region Styles
+const useStyles = makeStyles({
+  root: {
+    borderRadius: 20,
+    maxWidth: 800,
+    margin: "auto",
+  },
 
+  media: {
+    height: 140,
+    width: 320,
+    margin: "auto",
+  },
+});
 //#endregion
 
 export default function PdfCard(prop) {
   //#region HTML code
+  const classes = useStyles();
   return (
-    <div>
-      <Card
-          hoverable
-          style={{ width: 240 }}
-          cover={<img alt="" src="" />}>
-            <Meta title="" description="" />
-      </Card>,
-    </div>
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image={prop.prop.img}
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            {prop.prop.title}
+          </Typography>
+          <Typography
+            className="text-truncate"
+            variant="body2"
+            color="textSecondary"
+            component="p"
+          >
+            {prop.prop.content}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
   );
   //#endregion
 }
+//#endregion
