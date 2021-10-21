8cfd8b1a5049a8ea2c2351e37aa26bd571adaf01
Jonas Grooss Blaabjerg Christensen <jonasgross@hotmail.com>
2021-10-21 19:48:48 +0200

    styling FAQ

--- src/pages/FAQ.js
+++ src/pages/FAQ.js
@@ -1,18 +1,21 @@
 //#region Imports
 import React from 'react';
-import { Collapse } from 'antd';
+import { Collapse, Col, Row } from 'antd';
+
+import '../styles/faq.css';
 
 //#endregion
 
-const { Panel } = Collapse;
+// const { Panel } = Collapse;
 
-function callback(key) {
-  console.log(key);
-}
+// function callback(key) {
+//   console.log(key);
+// }
+
 // Tekst til FAQ
 var text = "Se parkeringsregler under dokumenter";
 var text2 = "GÆSTEPARKERINGS/DAGSKORT kort printes ud fra ejerforeningens hjemmeside til brug for gæster. Gæsteparkeringskortet skal påføres parkeringsdato, og kun én dato pr kort, og er gældende kun for denne dato. Gæsteparkeringskortene er gældende for parkering i områder med 30 minutters tidsbegrænset parkering . Dagskortet, der anvendes i Ahornlunden, er kun gældende på pladsen i Ahornlunden 1-9 plads nummer 3962. I Ahornlunden er det tilladt at parkere 1 time med korrekt indstillet P skive. Parkeringskortene administreres overordnet af Grundejerforeningen.";
-var text3 = "Affaldssortering hører under de enkelte ejerforeninger. Du kan finde deres regler på deres hjemmesider. Der er links til disse på Ejereningerne siden.";
+var text3 = "Affaldssortering hører under de enkelte ejerforeninger. Du kan finde deres regler på deres hjemmesider. Der er links til disse på Ejerforeningerne siden.";
 var text4 ="Alle må benytte legepladserne. Der må dog ikke luftes hunde på legepladserne. Læs skiltene og husk at ryd op efter dig";
 var text5 ="Du skal skrive til bestyrelsen via kontaktformularen på siden Kontakt os"
 
@@ -30,23 +33,71 @@ function FAQ() {
   //#region Html code
 
   return (
-    <Collapse defaultActiveKey={['0']} onChange={callback}>
-    <Panel header="Parkering som beboer" key="1">
-      <p>{text}</p>
-    </Panel>
-    <Panel header="Gæste parkering" key="2">
-      <p>{text2}</p>
-    </Panel>
-    <Panel header="Affaldsortering" key="3">
-      <p>{text3}</p>
-    </Panel>
-    <Panel header="Hvem må benytte legepladserne" key="4">
-      <p>{text4}</p>
-    </Panel>
-    <Panel header="Hvor skal jeg melde hvis noget går i stykker" key="5">
-      <p>{text5}</p>
-    </Panel>
-  </Collapse>
+        <div className="faqWrapper">
+          <Row type="flex">
+            <Col span={12}>
+              <div className="topLeft">
+                <div className="faqTextTopLeft">
+                  <h1> Affaldsortering </h1>
+                  <p> {text3} </p>
+                </div>
+              </div>
+            </Col>
+            <Col span={12}>
+              <div className="topRight">
+                <div className="faqTextTopRight">
+                  <h1> Gæste parkering </h1>
+                  <p> {text2} </p>
+                </div>
+              </div>
+            </Col>
+          </Row>
+          <Row type="flex">
+            <Col span={8}>
+              <div className="botLeft">
+                <div className="faqTextBotLeft">
+                  <h1> Parkering som beboer </h1>
+                  <p> {text} </p>
+                </div>
+              </div>
+            </Col>
+            <Col span={8}>
+              <div className="botMid">
+                <div className="faqTextBotMid">
+                  <h1> Hvem må benytte legepladserne </h1>
+                  <p> {text4} </p>
+                </div>
+              </div>
+            </Col>
+            <Col span={8}>
+              <div className="botRight">
+                <div className="faqTextBotRight">
+                  <h1> Hvor skal jeg melde hvis noget går i stykker </h1>
+                  <p> {text5} </p>
+                </div>
+              </div>
+            </Col>
+          </Row>
+        </div>
+
+
+  //   <Collapse defaultActiveKey={['0']} onChange={callback}>
+  //   <Panel header="Parkering som beboer" key="1">
+  //     <p>{text}</p>
+  //   </Panel>
+  //   <Panel header="Gæste parkering" key="2">
+  //     <p>{text2}</p>
+  //   </Panel>
+  //   <Panel header="Affaldsortering" key="3">
+  //     <p>{text3}</p>
+  //   </Panel>
+  //   <Panel header="Hvem må benytte legepladserne" key="4">
+  //     <p>{text4}</p>
+  //   </Panel>
+  //   <Panel header="Hvor skal jeg melde hvis noget går i stykker" key="5">
+  //     <p>{text5}</p>
+  //   </Panel>
+  // </Collapse>
   );
 
   //#endregion
